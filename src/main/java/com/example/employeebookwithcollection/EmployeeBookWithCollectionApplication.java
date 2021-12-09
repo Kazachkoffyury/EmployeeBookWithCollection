9d639ed2c650ff59044eb72bbdd8a1d492dc7479
Kazackoffyury <kazachkoff.yury@yandex.ru>
2021-12-09 13:09:24 +0300

    inintialize

---
+++ src/main/java/com/example/employeebookwithcollection/EmployeeBookWithCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeebookwithcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookWithCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookWithCollectionApplication.class, args);
+    }
+
+}
